99ac4ebee4db911b377d883563a59a4363911d0e
drivas <rivasdaniel1992@gmail.com>
2025-10-10 17:31:21 +0000

    fix: oopsie

--- client/src/components/Game.tsx
+++ client/src/components/Game.tsx
@@ -1,9 +1,10 @@
 import { Stack, Title } from "@mantine/core"
 import Board from "./Board"
-import type { Coordinates } from "../state/models"
+import type { Coordinates, Equipes } from "../state/models"
 import { useEffect, useState } from "react"
 import useGameStore from "../state/store"
 import { useSocket } from "../provider/socket"
+import { useSearchParams } from "react-router-dom";
 
 
 export enum GameEvent {
@@ -17,41 +18,53 @@ export enum GameEvent {
   CLEAR_CLAIM = 'ClearClaim',
 }
 
+const adminPass = "leboss420"
+
 export enum GAME_STATE {
   Lobby = 'La partie est en attente de commencer',
   InGame = 'La partie est commencée',
   EndGame = 'La partie est finit',
 }
+
+
 const Game = () => {
   const { socket } = useSocket();
 
   const reset = useGameStore((state) => state.reset)
   const claim = useGameStore((state) => state.claim)
   const [gameState, setGameState] = useState<GAME_STATE>(GAME_STATE.Lobby);
+  const [isAdmin, setIsAdmin] = useState<boolean>(false);
+
+  const [searchParams] = useSearchParams();
 
   useEffect(() => {
     if (socket) {
+      const mode = searchParams.get("mode");
+      if (mode && mode === adminPass) {
+        setIsAdmin(true)
+      }
+      if (isAdmin) {
+        socket.on(GameEvent.RESET_GAME, () => {
+          setGameState(GAME_STATE.Lobby)
+          reset();
+        });
 
+        socket.on(GameEvent.END_GAME, () => {
+          setGameState(GAME_STATE.EndGame)
+        });
 
-      socket.on(GameEvent.RESET_GAME, () => {
-        setGameState(GAME_STATE.Lobby)
-        reset();
-      });
-
-      socket.on(GameEvent.END_GAME, () => {
-        setGameState(GAME_STATE.EndGame)
-      });
-
-      socket.on(GameEvent.START_GAME, (data: any) => {
-        if (data.game_state = 1) {
-          setGameState(GAME_STATE.InGame);
-        }
-      });
+        socket.on(GameEvent.START_GAME, (data: any) => {
+          if (data.game_state = 1) {
+            setGameState(GAME_STATE.InGame);
+          }
+        });
+      }
 
       socket.on(GameEvent.UPDATE_TILE, (data: any) => {
         const coord: Coordinates = [data.x, data.y];
         claim(coord, data.team, new Date(data.lastCapture));
         const alert_string = `L'équipe ${data.team} à prossession de la case (${coord[0] + 1}, ${coord[1] + 1})`
+        console.log("JUSETE AVANT ALERT")
         alert(alert_string);
       });
 
@@ -66,8 +79,15 @@ const Game = () => {
       });
 
       socket.emit(GameEvent.UPDATE_ALL_TIMER);
-
-    }
+      return () => {
+        socket.off(GameEvent.RESET_GAME);
+        socket.off(GameEvent.END_GAME);
+        socket.off(GameEvent.START_GAME);
+        socket.off(GameEvent.UPDATE_TILE);
+        socket.off(GameEvent.UPDATE_ALL_TIMER);
+        socket.off(GameEvent.START_GAME);
+      };
+    };
   }, []);
 
   const StartGameButton = () => {
@@ -86,10 +106,12 @@ const Game = () => {
     <Stack>
       <Title order={2}>Grand jeu Connect4 Anims 2025</Title>
       <h1>ÉTAT DE LA PARTIE: {gameState}</h1>
-      <button onClick={StartGameButton}> StartGameButton</button>
-      <button onClick={ResetGameButton}> ResetGameButton</button>
-      <Board />
-    </Stack >
+      {isAdmin && (
+        <>
+          <button onClick={StartGameButton}>Start</button>
+          <button onClick={ResetGameButton}>Reset</button>
+        </>
+      )}< Board isAdmin={isAdmin} /> </Stack >
   )
 }
 
